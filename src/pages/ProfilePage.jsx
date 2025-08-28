bcf88c177caeec8eb69998b0867b46ac0b7672a6
weic777 <anna0826777@gmail.com>
2025-08-28 19:26:39 +0800

    0828

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -22,6 +22,13 @@ import htmlLogo from '../assets/html-logo.svg';
 import cssLogo from '../assets/css-logo.svg';
 import jsLogo from '../assets/js-logo.svg';
 import idLogo from '../assets/id-logo.svg';
+import WorkLogo1 from '../assets/中國時報.svg';
+import WorkLogo2 from '../assets/康健雜誌.svg';
+import WorkLogo3 from '../assets/新光三越.svg';
+import WorkLogo4 from '../assets/HiSKIO.svg';
+import WorkLogo5 from '../assets/樂天市場.svg';
+import WorkLogo6 from '../assets/甲桂林廣告.svg';
+
 import { useEffect, useRef, useState } from 'react';
 
 
@@ -32,27 +39,27 @@ function ProfilePage() {
   const targetYRef = useRef(150);
 
 
-useEffect(() => {
-  const handleScroll = () => {
-    if (!wrapperRef.current) return;
-    const rect = wrapperRef.current.getBoundingClientRect();
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!wrapperRef.current) return;
+      const rect = wrapperRef.current.getBoundingClientRect();
 
-    // 當容器還沒到達視窗時，大頭貼往上移動，滾回來就往下
-    // 這裡 150 是初始位移，你可以改成自己想要的最大位移
-targetYRef.current = Math.min(Math.max(150 - rect.top + 50, 0), 200);
-  };
+      // 當容器還沒到達視窗時，大頭貼往上移動，滾回來就往下
+      // 這裡 150 是初始位移，你可以改成自己想要的最大位移
+      targetYRef.current = Math.min(Math.max(150 - rect.top + 50, 0), 200);
+    };
 
-  const animate = () => {
-    // 緩動更新，但可以把比例調大，快一點
-    setTranslateY(prev => prev + (targetYRef.current - prev) * 0.3);
-    requestAnimationFrame(animate);
-  };
+    const animate = () => {
+      // 緩動更新，但可以把比例調大，快一點
+      setTranslateY(prev => prev + (targetYRef.current - prev) * 0.3);
+      requestAnimationFrame(animate);
+    };
 
-  window.addEventListener('scroll', handleScroll);
-  animate();
+    window.addEventListener('scroll', handleScroll);
+    animate();
 
-  return () => window.removeEventListener('scroll', handleScroll);
-}, []);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
 
 
@@ -80,109 +87,56 @@ targetYRef.current = Math.min(Math.max(150 - rect.top + 50, 0), 200);
       title: '中國時報',
       desc:
         'Gain the confidence to build anything you envision, transforming motion, interaction, and design principles into second nature.',
-      img: 'https://picsum.photos/720/720?random=12',
-      icon: (
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"
-          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M6 3h12l4 6-10 13L2 9Z" />
-          <path d="M11 3 8 9l4 13 4-13-3-6" />
-          <path d="M2 9h20" />
-        </svg>
-      ),
+      logo:  WorkLogo1 ,
+      date: '2022',
     },
     {
       title: '康健雜誌',
       desc:
         'Master CSS animations from your very first set of @keyframes right through to things no one else ever teaches you.',
-      img: 'https://picsum.photos/720/720?random=17',
-      icon: (
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"
-          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <rect width="18" height="18" x="3" y="3" rx="2" />
-          <path d="M7 3v18" />
-          <path d="M3 7.5h4" />
-          <path d="M3 12h18" />
-          <path d="M3 16.5h4" />
-          <path d="M17 3v18" />
-          <path d="M17 7.5h4" />
-          <path d="M17 16.5h4" />
-        </svg>
-      ),
+      
+      logo: WorkLogo2 ,
+      date: '2022',
     },
     {
       title: '新光三越',
       desc:
         'Shaders on a budget. Learn how to use noise to your advantage whilst making flames and stickers.',
-      img: 'https://picsum.photos/720/720?random=19',
-      icon: (
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"
-          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3" />
-        </svg>
-      ),
+      
+      logo: WorkLogo3,
+      date: '2022',
     },
     {
       title: 'HiSKIO',
       desc:
         'Take your users on a journey with the joy of tasteful scroll animation. You might not even need JavaScript.',
       img: 'https://picsum.photos/720/720?random=42',
-      icon: (
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"
-          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M19 17V5a2 2 0 0 0-2-2H4" />
-          <path d="M8 21h12a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v1a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v2a1 1 0 0 0 1 1h3" />
-        </svg>
-      ),
+      
+      logo: WorkLogo4 ,
+      date: '2022',
     },
     {
       title: '樂天市場',
       desc:
         'Grasp how to tame the pixel playground and when to do so. Whilst building with "Performance Driven Development".',
       img: 'https://picsum.photos/720/720?random=128',
-      icon: (
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"
-          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <circle cx="13.5" cy="6.5" r=".5" fill="currentColor" />
-          <circle cx="17.5" cy="10.5" r=".5" fill="currentColor" />
-          <circle cx="8.5" cy="7.5" r=".5" fill="currentColor" />
-          <circle cx="6.5" cy="12.5" r=".5" fill="currentColor" />
-          <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z" />
-        </svg>
-      ),
+      
+      logo: WorkLogo5 ,
+      date: '2022',
     },
     {
       title: '甲桂林廣告',
       desc:
         'Do you really need a library for that? Sometimes stepping back and rethinking the problem yields a nifty solution.',
-      img: 'https://picsum.photos/720/720?random=56',
-      icon: (
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"
-          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="m21.64 3.64-1.28-1.28a1.21 1.21 0 0 0-1.72 0L2.36 18.64a1.21 1.21 0 0 0 0 1.72l1.28 1.28a1.2 1.2 0 0 0 1.72 0L21.64 5.36a1.2 1.2 0 0 0 0-1.72" />
-          <path d="m14 7 3 3" />
-          <path d="M5 6v4" />
-          <path d="M19 14v4" />
-          <path d="M10 2v2" />
-          <path d="M7 8H3" />
-          <path d="M21 16h-4" />
-          <path d="M11 3H9" />
-        </svg>
-      ),
+      
+      logo:  WorkLogo6 ,
+      date: '2022',
     },
     {
       title: '勞動部',
       desc:
         "It's not all just easings and compositions. Time plays a crucial part in various UI patterns that might not seem obvious at first.",
-      img: 'https://picsum.photos/720/720?random=39',
-      icon: (
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"
-          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M5 22h14" />
-          <path d="M5 2h14" />
-          <path d="M17 22v-4.172a2 2 0 0 0-.586-1.414L12 12l-4.414 4.414A2 2 0 0 0 7 17.828V22" />
-          <path d="M7 2v4.172a2 2 0 0 0 .586 1.414L12 12l4.414-4.414A2 2 0 0 0 17 6.172V2" />
-        </svg>
-      ),
+      date: '2022',
     },
   ];
   const [activeExp, setActiveExp] = useState(expItems.length - 1);
@@ -350,14 +304,23 @@ targetYRef.current = Math.min(Math.max(150 - rect.top + 50, 0), 200);
               onMouseEnter={() => setActiveExp(i)}
             >
               <article className="exp-article">
-                <h3 lang={/^[A-Za-z]+$/.test(item.title) ? "en" : "zh"}>
-                  {item.title}
-                </h3>
-                <p>{item.desc}</p>
-                {item.icon}
-                {/* <a href="#"><span>Watch now</span></a> */}
-                <img src={item.img} alt="" />
-              </article>
+  <h3 lang={/^[A-Za-z]+$/.test(item.title) ? "en" : "zh"}>
+    {item.logo && <img src={item.logo} alt={`${item.title} logo`} className="exp-logo" />}
+    <span className="exp-title-text">{item.title}</span>
+  </h3>
+
+  <div className="exp-info">
+    <div className="exp-line"></div>
+    <div className="exp-position">平面設計</div>
+    <div className="exp-year">{item.date}</div>
+  </div>
+
+  <p>{item.desc}</p>
+  {item.icon}
+  <img src={item.img} alt="" />
+</article>
+
+
             </li>
           ))}
         </ul>
